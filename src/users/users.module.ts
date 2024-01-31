ad40e04f82402a95f448b74c04151c9b423cae1e
MateusFM <mateusmatias48@gmail.com>
2024-01-31 09:46:25 -0300

    auth

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { AuthModule } from 'src/modules/auth/auth.module';
 import { PrismaService } from './prisma.service';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, PrismaService],
   exports: [PrismaService],
 })
 export class UsersModule {}
